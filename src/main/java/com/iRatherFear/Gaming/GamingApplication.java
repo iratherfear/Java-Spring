6b1274bd01aa8d8c78942905fa11e026890bde34
aman <amanadaf4ever@gmail.com>
2024-04-04 10:14:58 +0530

    Little more about beans and tags.

--- src/main/java/com/iRatherFear/Gaming/GamingApplication.java
+++ src/main/java/com/iRatherFear/Gaming/GamingApplication.java
@@ -11,10 +11,10 @@ public class GamingApplication {
 		System.out.println("Name bean: " + context.getBean("name"));
 		System.out.println("Age bean: " + context.getBean("age"));
 		System.out.println("Person bean: " + context.getBean("person"));
-		System.out.println("Address bean: " + context.getBean("address"));
+//		System.out.println("Address bean: " + context.getBean("address"));
 		System.out.println("Address bean with different name than function name using name tag in bean annotation: " + context.getBean("getAddress"));
 		System.out.println("Person bean with values derived from other beans: " + context.getBean("getPersonUsingExistingBean"));
-
+		System.out.println("Person2 bean with parameteised method: " + context.getBean("personParameterised"));
 	}
 
 }
--- src/main/java/com/iRatherFear/Gaming/HelloWorld/HelloWorldConfiguration.java
+++ src/main/java/com/iRatherFear/Gaming/HelloWorld/HelloWorldConfiguration.java
@@ -1,9 +1,11 @@
 package com.iRatherFear.Gaming.HelloWorld;
 
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) {}
+//record Person(String name, int age) {}
+record Person(String name, int age, Address address){}
 record Address(String firstLine, String city) {}
 @Configuration
 public class HelloWorldConfiguration {
@@ -17,21 +19,35 @@ public class HelloWorldConfiguration {
     }
     @Bean
     public Person person() {
-        return new Person("Sheldon", 33);
+        return new Person("Sheldon", 33, new Address("Pasadena", "California"));
     }
 
     @Bean
     public Person getPersonUsingExistingBean() {
-        return new Person(name(), age());
+        return new Person(name(), age(), address());
     }
 
+    /*
+    * The bean is created such a way that all the paramters of this function
+    * is name of some other beans in this file.
+    * When we call this bean, values corresponding to each bean will be called.
+    * E.g. There are three parameters name, age and getAddress. When we call this
+    * bean, beans with these name will be called.
+    *   NOTE: You can name this variable anything. But no two bean should repeat the same data type.
+    * */
     @Bean
-    public Address address() {
-        return new Address("21B Baker Stree", "London");
+    public Person personParameterised(String name, int age, Address getAddress) {
+        return new Person(name, age, getAddress);
     }
-
     @Bean(name = "getAddress")
-    public Address address2() {
+    public Address address() {
         return new Address("High Street", "Balewadi");
     }
+
+//    @Bean(name = "getAddress2")
+//    public Address address2() {
+//        return new Address("High Street", "Balewadi");
+//    }
+
+
 }
